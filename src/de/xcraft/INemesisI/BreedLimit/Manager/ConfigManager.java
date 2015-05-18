7d1a29777400d4b59276373ac237469c961d03a8
Voronwe <Bronwe@users.noreply.github.com>
2015-05-18 15:55:48 +0200

    Removeing licence

--- src/de/xcraft/INemesisI/BreedLimit/Manager/ConfigManager.java
+++ src/de/xcraft/INemesisI/BreedLimit/Manager/ConfigManager.java
@@ -20,7 +20,6 @@ public class ConfigManager extends XcraftConfigManager {
 
 	public int radius;
 	public final Map<EntityType, Integer> limits = new HashMap<EntityType, Integer>();
-	public final Map<EntityType, Integer> prices = new HashMap<EntityType, Integer>();
 	private final Map<EntityType, String> messages = new HashMap<EntityType, String>();
 
 	public ConfigManager(XcraftBreedLimit plugin) {
@@ -37,27 +36,12 @@ public class ConfigManager extends XcraftConfigManager {
 				limits.put(EntityType.valueOf(key), cs.getInt(key));
 			}
 		}
-		cs = config.getConfigurationSection("Breeding.Licence");
-		for (String key : cs.getKeys(false)) {
-			if (EntityType.valueOf(key) != null) {
-				prices.put(EntityType.valueOf(key), cs.getInt(key));
-			}
-		}
 		cs = config.getConfigurationSection("Breeding.Message");
 		for (String key : cs.getKeys(false)) {
 			if (key.equals("default") || EntityType.valueOf(key) != null) {
 				EntityType type = key.equals("default") ? EntityType.UNKNOWN : EntityType.valueOf(key);
 				messages.put(type, cs.getString(key));
 			}
-		}
-		Map<String, Map<EntityType, Integer>> licences = new HashMap<String, Map<EntityType, Integer>>();
-		for (String player : data.getKeys(false)) {
-			Map<EntityType, Integer> list = new HashMap<EntityType, Integer>();
-			cs = data.getConfigurationSection(player);
-			for (String entityType : cs.getKeys(false)) {
-				list.put(EntityType.valueOf(entityType), cs.getInt(entityType));
-			}
-			licences.put(player, list);
 		}
 		((PluginManager) plugin.getPluginManager()).setLicences(licences);
 		this.radius = config.getInt("Breeding.Radius", 2);
@@ -66,9 +50,6 @@ public class ConfigManager extends XcraftConfigManager {
 	@Override
 	public void save() {
 		PluginManager pManager = (PluginManager) plugin.getPluginManager();
-		for (String player : pManager.getLicences().keySet()) {
-			data.set(player, pManager.getLicence(player));
-		}
 		try {
 			data.save(dataFile);
 		} catch (IOException e) {
